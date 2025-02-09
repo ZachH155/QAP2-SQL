904c1afa94021066054f1e0ec02faf43f18aec1e
ZachH155 <150450377+ZachH155@users.noreply.github.com>
2025-02-08 20:44:23 -0330

    Forgot to write "NOT NULL"'s

--- Problem1.sql
+++ Problem1.sql
@@ -1,21 +1,21 @@
 -- TABLE CREATION
 CREATE TABLE students (
-	id SERIAL PRIMARY KEY,
-	first_name VARCHAR(20),
-  last_name VARCHAR(20),
-  email VARCHAR(40),
+	id SERIAL PRIMARY KEY NOT NULL, 
+	first_name VARCHAR(20), 
+	last_name VARCHAR(20), 
+	email VARCHAR(40), 
 	school_enrollment_date VARCHAR(8)
 )
 
 CREATE TABLE professors (
-	id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY NOT NULL,
 	first_name VARCHAR(20),
-  last_name VARCHAR(20),
-  department VARCHAR(20)
+  	last_name VARCHAR(20),
+  	department VARCHAR(20)
 )
   
 CREATE TABLE courses (
-	id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY NOT NULL,
 	course_name VARCHAR(20),
 	course_description VARCHAR(100),
 	professor_id integer REFERENCES professors (id)
