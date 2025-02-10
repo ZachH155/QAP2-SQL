d60d2c4807db4562afca7940e73a39eff8c3578e
ZachH155 <150450377+ZachH155@users.noreply.github.com>
2025-02-09 21:18:47 -0330

    Added data insertion queries

--- Problem1.sql
+++ Problem1.sql
@@ -29,3 +29,23 @@ CREATE TABLE enrollments (
 )
 
 -- DATA INSERTION
+INSERT INTO students (first_name, last_name, email, school_enrollment_date)
+VALUES ('Corneleus', 'Johnson', 'cornyjohn@gmail.com', CURRENT_DATE),
+	   ('Zeberflorb', 'Smith', 'florbinit@gmail.com', CURRENT_DATE),
+	   ('Dave', 'Davidson', 'davedson@gmail.com', CURRENT_DATE),
+	   ('Quavius', 'Babarlathew', 'barbar@gmail.com', CURRENT_DATE),
+	   ('Goku', 'Son', 'heyitsme@gmail.com', CURRENT_DATE)
+
+INSERT INTO professors (first_name, last_name, department)
+VALUES ('Glorbius', 'Goboban', 'Science'),
+	   ('Frank', 'Frankfurt', 'Math'),
+	   ('Vera', 'Davidson', 'Socials'),
+	   ('Demetrius', 'Babarlathew', 'English'),
+	   ('Alexandra', 'Ivanovich', 'French')
+
+INSERT INTO courses (course_name, course_description, professor_id)
+VALUES ('English', 'Writing essays about things you didnt know exist until the teacher gives you the test', 4),
+	   ('Math', '"My method is better!" -Math teachers', 2),
+	   ('Socials', 'Youll be hearing more history about the teacher than about the world', 3),
+	   ('Science', 'Science!... The teacher is harder to deal with than anything', 1),
+	   ('French', 'Vous vous souviendrez de tout le français que vous apprendrez!... Jusquau prochain test', 5)
